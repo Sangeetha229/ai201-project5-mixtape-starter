14bed675360041f081d83f87afee9912a2f0ff4d
s_e229 <s_e229@txstate.edu>
2026-07-05 22:57:10 -0500

    fix: Included all songs in the playlist

--- services/playlist_service.py
+++ services/playlist_service.py
@@ -62,8 +62,9 @@ def get_playlist_songs(playlist_id: str) -> list[dict]:
         .order_by(asc(playlist_entries.c.position))
         .all()
     )
-
-    return [song.to_dict() for song in songs[:-1]]
+    # ✅ (BUG FIX 5 : Include  all songs in the playlist)   
+    return [song.to_dict() for song in songs[:]]
+    # return [song.to_dict() for song in songs[:-1]]
 
 
 def get_playlist(playlist_id: str) -> dict:
